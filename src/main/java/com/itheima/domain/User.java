4c9ce0d23786ef82d9d7bbedd8195930b500bc57
xiaoke <15072770488@163.com>
2019-08-28 19:35:28 +0800

    新建user

---
+++ src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,11 @@
+package com.itheima.domain;
+
+/**
+ * @program: day826_Lucene
+ * @description:
+ * @author:
+ * @create: 2019-08-28 19:34
+ **/
+public class User {
+    private String username;
+}
